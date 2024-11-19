55b5dba3cd51af013d1604d69db102021b3e488a
kjvits <kjvita@gbox.ncf.edu.ph>
2024-11-20 00:37:20 +0800

    update hobbies

--- src/Pages/Hobbies.jsx
+++ src/Pages/Hobbies.jsx
@@ -10,16 +10,16 @@ function Hobbies() {
       <p>Here are a few of the things I enjoy doing in my free time.</p>
       <div className="hobbies-images">
         {/* Replace these <img> tags with your images */}
-        <img src="img/vsvs.jpg" alt="Hobby 1" />
-        <img src="img/val.png" alt="Hobby 2" />
-        <img src="img/wuwa.webp" alt="Hobby 3" />
-        <img src="img/lol.png" alt="Hobby 4" />
-        <img src="img/acad.webp" alt="Hobby 5" />
-        <img src="img/drift.webp" alt="Hobby 6" />
-        <img src="img/nano.webp" alt="Hobby 7" />
-        <img src="img/swordhound.webp" alt="Hobby 8" />
-        <img src="img/orv.webp" alt="Hobby 9" />
-        <img src="img/eleceed.webp" alt="Hobby 10" />
+        <img src="src/assets/img/vsvs.jpg" alt="Hobby 1" />
+        <img src="src/assets/img/val.png" alt="Hobby 2" />
+        <img src="src/assets/img/wuwa.webp" alt="Hobby 3" />
+        <img src="src/assets/img/lol.png" alt="Hobby 4" />
+        <img src="src/assets/img/acad.webp" alt="Hobby 5" />
+        <img src="src/assets/img/drift.webp" alt="Hobby 6" />
+        <img src="src/assets/img/nano.webp" alt="Hobby 7" />
+        <img src="src/assets/img/swordhound.webp" alt="Hobby 8" />
+        <img src="src/assets/img/orv.webp" alt="Hobby 9" />
+        <img src="src/assets/img/eleceed.webp" alt="Hobby 10" />
       </div>
       <Link to="/contact" className="btn btn-primary">Get In Touch</Link>
     </div>
