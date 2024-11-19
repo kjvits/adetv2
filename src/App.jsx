1c38acfbd6edac7babdf9e2fa873b521cfa1c1f1
kjvits <kjvita@gbox.ncf.edu.ph>
2024-11-20 01:03:30 +0800

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Pages/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Education from './Pages/Education';
 import Hobbies from './Pages/Hobbies';
-import Navbar from './Pages/Navbar';
 import Contact from './Pages/Contact';
 
 function App() {
   return (
     <Router>
+      {/* Navbar is always visible */}
       <Navbar />
+
+      {/* Main routing for different pages */}
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Home page path should be '/' */}
-        <Route path="/about" element={<About />} /> {/* About page path should be '/about' */}
-        <Route path="/education" element={<Education />} /> {/* Education page path should be '/education' */}
-        <Route path="/hobbies" element={<Hobbies />} /> {/* Hobbies page path should be '/hobbies' */}
-        <Route path="/contact" element={<Contact />} /> {/* Ensure this path is included */}
+        {/* Route for the home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Route for the about page */}
+        <Route path="/about" element={<About />} />
+
+        {/* Route for the education page */}
+        <Route path="/education" element={<Education />} />
+
+        {/* Route for the hobbies page */}
+        <Route path="/hobbies" element={<Hobbies />} />
+
+        {/* Route for the contact page */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
