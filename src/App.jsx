25ba1c87439d0d04ca63e91c2a5941c9b9e242e2
kjvits <kjvita@gbox.ncf.edu.ph>
2024-11-20 14:24:36 +0800

    update appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ import Education from './Pages/Education';
 import Hobbies from './Pages/Hobbies';
 import Contact from './Pages/Contact';
 
-// Optional: 404 Page Not Found Component
-import NotFound from './Pages/NotFound'; 
-
 function App() {
   return (
     <Router>
@@ -32,9 +29,6 @@ function App() {
 
         {/* Route for the contact page */}
         <Route path="/contact" element={<Contact />} />
-
-        {/* Catch-all route for any invalid paths */}
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
