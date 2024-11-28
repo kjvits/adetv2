910fc277fd20b4a2b09211fd256b1e575a9b32c1
kjvits <kjvita@gbox.ncf.edu.ph>
2024-11-28 17:36:59 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Pages/Navbar';
+import Navbar from './Pages/navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Education from './Pages/Education';
